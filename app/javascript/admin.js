3dfadd0225b44a05ed529fc38d6ae7e693009d0a
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2024-03-22 17:13:25 -0300

    gerador de codigo e mascara valor

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -1,9 +1,11 @@
 import "./initializers";
+import "jquery-mask-plugin";
 require("jquery")
 require("@nathanvda/cocoon")
 import 'bootstrap/dist/js/bootstrap.bundle';
 import "./libs/admin/sidebarmenu";
 import "./libs/admin/app.min";
 import SimpleBar from 'simplebar';
-//import "./libs/admin/dashboard";
 import '@fortawesome/fontawesome-free/js/all';
+
+$("#product_skus_attributes_0_price_table, #product_skus_attributes_0_price_listing").mask('#.##0,00', {reverse: true, placeholder: '0,00'});
--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,3 +1,16 @@
 class Sku < ApplicationRecord
 	belongs_to :product
+	before_create :generate_ean13
+
+	validates :code, uniqueness: true
+
+	private
+
+	def generate_ean13
+		code = rand.to_s[2..13] 
+		while !Sku.find_by(code: code).nil?
+			code = rand.to_s[2..13] 
+		end
+		self.code = code
+	end
 end
