48a06cc7356e2b75bcd9e0926a6fff73aed93641
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2024-03-25 21:40:01 -0300

    autorização Bearer token

--- app/auth/authorization.rb
+++ app/auth/authorization.rb
@@ -1,27 +1,59 @@
+# class Authorization
+#   	def initialize(request)
+#     	@token = request.headers[:Authorization]
+#   	end
+
+#   	def current_user
+#     	@current_user ||= fetch_current_user if @token.present?
+# 	rescue StandardError => e
+#     	Rails.logger.error("Search user Error: #{e.message}")
+#     	nil
+#   	end
+
+#   	private
+
+#   	def fetch_current_user
+
+#   		decodeToken = JsonWebToken.decode(@token)
+
+#   		user_id = decodeToken[:user_id]
+
+#   		user = User.find_by(id: user_id, status: 0)
+    	
+#     	user = user if user && decodeToken[:expires_in].to_i > Time.now.to_i 
+    	
+#     	user
+#   	end
+# end
+
 class Authorization
-  	def initialize(request)
-    	@token = request.headers[:Authorization]
-  	end
+  def initialize(request)
+    @token = extract_token(request)
+  end
 
-  	def current_user
-    	@current_user ||= fetch_current_user if @token.present?
-	rescue StandardError => e
-    	Rails.logger.error("Search user Error: #{e.message}")
-    	nil
-  	end
+  def current_user
+    @current_user ||= fetch_current_user if @token.present?
+  rescue StandardError => e
+    Rails.logger.error("Search user Error: #{e.message}")
+    nil
+  end
 
-  	private
+  private
 
-  	def fetch_current_user
+  def extract_token(request)
+    authorization_header = request.headers['Authorization']
+    return nil unless authorization_header.present?
 
-  		decodeToken = JsonWebToken.decode(@token)
+    token_type, token = authorization_header.split(' ')
+    return token if token_type == 'Bearer'
 
-  		user_id = decodeToken[:user_id]
+    nil
+  end
 
-  		user = User.find_by(id: user_id, status: 0)
-    	
-    	user = user if user && decodeToken[:expires_in].to_i > Time.now.to_i 
-    	
-    	user
-  	end
+  def fetch_current_user
+    decodeToken = JsonWebToken.decode(@token)
+    user_id = decodeToken[:user_id]
+    user = User.find_by(id: user_id, status: 0)
+    user if user && decodeToken[:expires_in].to_i > Time.now.to_i
+  end
 end
